19d98510cd06ee16d64d3b44dbc87b6db3fc1dc5
yongseopK <kk0@kakao.com>
2024-04-08 10:37:12 +0900

    헤더 수정

--- src/components/layout/jsx/Header.jsx
+++ src/components/layout/jsx/Header.jsx
@@ -2,13 +2,10 @@ import React, {useState} from 'react';
 import '../scss/Header.scss';
 import {Link} from "react-router-dom";
 import {MdLogin} from "react-icons/md";
-import {IconContext} from "react-icons";
 import {IoMenu} from "react-icons/io5";
-<<<<<<< Updated upstream
-=======
 import {getLogin} from "../../../util/login-util.jsx";
 import Dropdown from "./Dropdown.jsx";
->>>>>>> Stashed changes
+
 
 const Header = () => {
 
@@ -22,13 +19,8 @@ const Header = () => {
                         <Link to="/" className={"home-link"}>Clean Cherry Blossom</Link>
                     </div>
                     {
-<<<<<<< Updated upstream
-                        isLogin ? (
-                            <IoMenu className={"btn-menu-mobile"}/>
-=======
                         login ? (
                             <Dropdown login={login}/>
->>>>>>> Stashed changes
                         ) : (
                             <Link to={"/login"}>
                                 <MdLogin className={"btn-login-mobile"}/>
